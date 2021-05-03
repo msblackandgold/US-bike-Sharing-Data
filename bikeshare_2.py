e0d173e89e0f9b42508d108ef844bfa01d48fce9
Michael <mike.saul@symetra.com>
2021-05-03 13:04:56 -0700

    Added clarifying documentation to common times

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -88,7 +88,7 @@ def load_data(city, month, day):
     return df
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent months, days and hour of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
